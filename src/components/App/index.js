a10253b99d8f6592f2cf99a827cf7c03436c02f2
Pierre Beaufils <pierre.beaufils@outlook.fr>
2020-10-06 19:02:50 +0200

    css on quizz cards on homepage

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
-import './styles.css';
+import './styles.scss';
 import { Switch, Route } from 'react-router-dom';
 
 import Page from 'src/components/Page';
--- src/components/Home/QuizCard.js
+++ src/components/Home/QuizCard.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import star from 'src/assets/images/star.png';
+import picture from 'src/assets/images/card-oldd.jpg';
 
 import './home.scss';
 
@@ -8,7 +10,16 @@ import './home.scss';
 const QuizCard = ({ title, description, id }) => (
   <Link to={`/quiz/${id}`}>
     <div className="quiz-card">
-      <div className="quiz-card--header">{title}</div>
+      <div className="quiz-card--header">
+        <img src={picture} alt="" className="image" />
+        <div className="quiz-card--header-content">
+          <div className="quiz-card--title">{title}</div>
+          <div className="quiz-card--stars">
+            <img src={star} alt="" width="20px" />
+            <img src={star} alt="" width="20px" />
+          </div>
+        </div>
+      </div>
       <div className="quiz-card--description">{description}</div>
     </div>
   </Link>
