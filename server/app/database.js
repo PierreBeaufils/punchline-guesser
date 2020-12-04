dbcae15806ee5e238a96cb02a3a225be0260135b
Pierre Beaufils <pierre.beaufils@outlook.fr>
2020-12-04 22:06:37 +0100

    changed .env datas

--- server/app/database.js
+++ server/app/database.js
@@ -2,7 +2,7 @@ const {
     Sequelize
 } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.PG_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     logging: false, //Disable requests views in terminal
     define: {
         underscored: true, //Enable snake_case naming
