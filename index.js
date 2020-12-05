0db8a0e827c87ee73ed1c6e84959cd9ed36d70ab
Pierre Beaufils <pierre.beaufils@outlook.fr>
2020-12-05 21:09:30 +0100

    path

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ if (ENV === 'production') {
 */
 if (process.env.NODE_ENV === 'production') {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, 'client/dist')));
+    server.use(express.static(path.join(__dirname, 'client/dist')));
     // Handle React routing, return all requests to React app
-    app.get('*', function (req, res) {
+    server.get('*', function (req, res) {
         res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
     });
 }
